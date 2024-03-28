32972c837a524d263dd9c17311557acc747b0375
T_DaMER <dpancov64@gmail.com>
2024-03-28 22:45:02 +0300

    Add deletion

--- src/screens/DetailsPage.tsx
+++ src/screens/DetailsPage.tsx
@@ -1,24 +1,41 @@
 import { navigate } from 'wouter-preact/use-browser-location'
-import { useAtomValue } from 'jotai'
+import { useAtom } from 'jotai'
+import { useCallback } from 'preact/hooks'
 import VacTable from 'components/VacTable'
 import calculateCompletedVacs from 'helpers/calculateCompletedVacs'
 import nameToBirthDateStorage from 'atoms/nameToBirthDateStorage'
 
 export default function ({ name }: { name: string }) {
-  const birthDate = useAtomValue(nameToBirthDateStorage)[name]
+  const [patientsData, setPatientsData] = useAtom(nameToBirthDateStorage)
+  const birthDate = patientsData[name]
   const { completedPercent, daysFromBirth } = calculateCompletedVacs(birthDate)
 
   const today = new Date().toLocaleDateString()
   const birth = new Date(birthDate).toLocaleDateString()
 
+  const deleteEntry = useCallback(() => {
+    if (!patientsData[name]) return
+
+    delete patientsData[name]
+
+    navigate('/')
+    setPatientsData(patientsData)
+  }, [name, patientsData, setPatientsData])
+
   return (
     <div className="flex flex-col gap-x-2">
-      <a
-        onClick={() => navigate('/')}
-        className="cursor-pointer hover:opacity-50 transition-opacity"
-      >
-        ◄ Go back
-      </a>
+      <div className="flex justify-between items-center">
+        <a
+          onClick={() => navigate('/')}
+          className="cursor-pointer hover:opacity-50 transition-opacity"
+        >
+          ◄ Go back
+        </a>
+
+        <a className="text-red-400 cursor-pointer" onClick={deleteEntry}>
+          Delete
+        </a>
+      </div>
       <VacTable completedPercent={completedPercent} />
       <span>Name: {name}</span>
       <span>Birth date: {birth}</span>
